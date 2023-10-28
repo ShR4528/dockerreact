05bd07a7edd720643c3e9d9ffd07b828ea4568a2
Shr4528 <Shr4528@gmail.com>
2023-10-28 13:21:32 -0500

    axios

--- server/index.js
+++ server/index.js
@@ -1,57 +1,53 @@
-const keys = require('./keys');
+const keys = require("./keys");
 
-//Express Application setup
-
-const express = require('express');
-const bodeParser = require('body-parser');
-const cors = require('cors');
+// Express Application setup
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
-app.use(bodeParser.json());
-
+app.use(bodyParser.json());
 
 // Postgres client setup
-
 const { Pool } = require("pg");
 const pgClient = new Pool({
     user: keys.pgUser,
     host: keys.pgHost,
     database: keys.pgDatabase,
     password: keys.pgPassword,
-    port: keys.pgPort,
+    port: keys.pgPort
 });
 
-pgClient.on('connect', (client) => {
-    client.query('Create Table if not exists values (number int)')
+pgClient.on("connect", client => {
+    client
+        .query("CREATE TABLE IF NOT EXISTS values (number INT)")
         .catch(err => console.log("PG ERROR", err));
 });
 
-// Express route def
-
-app.get('/', (req, res) => {
+//Express route definitions
+app.get("/", (req, res) => {
     res.send("Hi");
 });
 
 // get the values
-app.get('/values/all', async (req, res) => {
-    const values = await pgClient.query("SELECT * From values");
+app.get("/values/all", async (req, res) => {
+    const values = await pgClient.query("SELECT * FROM values");
+
     res.send(values);
 });
 
-// insert value
+// now the post -> insert value
 app.post("/values", async (req, res) => {
-
     if (!req.body.value) res.send({ working: false });
 
-    pgClient.query("Insert into values(numbers) Values($1)", [req.body.value]);
+    pgClient.query("INSERT INTO values(number) VALUES($1)", [req.body.value]);
 
     res.send({ working: true });
 });
 
 app.listen(5000, err => {
     console.log("Listening");
-
-})
+});
 
 
